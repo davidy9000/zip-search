69a40053cdb44c499e16f439160fc9cccd52c35f
Melissa Lynch <melissalynch@Melissas-MacBook.local>
2020-01-09 18:27:48 -0500

    done with zip-search

--- src/components/ZipComponent.js
+++ src/components/ZipComponent.js
@@ -4,63 +4,63 @@ class ZipCode extends Component {
     constructor(props){
         super(props);
         this.state = {
-            usState: props.usState,
-            lat: props.lat,
-            lon: props.lon, 
-            population: props.population,
-            wages: props.wages,
             apiData: []
         }
-        //this.apiData = [];
     }
 
     handleSearchClick = () => {
-        let linkToAPI = 'http://ctp-zip-api.herokuapp.com/zip/'+document.getElementById("search-text").value;
+        let linkToAPI= 'http://ctp-zip-api.herokuapp.com/zip/'+document.getElementById("search-text").value;
         fetch(linkToAPI)
             .then((response) => {
+                if(response.status === 404){
+                    return;
+                }
                 return response.json();
             })
-            .then((myJson) => {
-                this.setState({apiData: myJson});/*
-                let data = myJson;
-                console.log(data[0])
-                    this.setState({population: data[0].EstimatedPopulation,
-                        usState: data[0].State, lat: data[0].Lat, 
-                        lon: data[0].Long,wages: data[0].TotalWages});*/
+            .then((data) => {
+                this.setState({apiData: data});
             })
             .catch((error) => {
                 console.error('Error:', error);
             });
     }
 
-    makeTable = () =>{
-        //console.log(this.state.apiData);
+    makeTable = () => {
         let currData = this.state.apiData;
         let table = [];
-        let row = [];
-        for(let i = 0; i < currData.length; i++){
-            row.push(<td>
-                <div className = "LocationCard">
-                <div>State: {currData[i].State}</div>
-                <div>Location: {currData[i].Lat}, {currData[i].Long}</div>
-                </div></td>)
+        //currData is undefined when we get 404 error
+        if(currData===undefined){
+            table.push(<tr>No Results</tr>);
+            return table;
+        } else {
+            let rowHead = [];
+            let rowData = [];
+            for(let i = 0; i < currData.length; i++){
+                rowHead.push(<th>{currData[i].City},{currData[i].State}</th>)
+                rowData.push(<td>
+                    <p>State: {currData[i].State}</p>
+                    <p>Location: {currData[i].Lat}, {currData[i].Long}</p>
+                    <p>Population: {currData[i].EstimatedPopulation}</p>
+                    <p>Wages: {currData[i].TotalWages}</p>
+                    </td>);
+            }
+            table.push(<tr>{rowHead}</tr>);
+            table.push(<tr>{rowData}</tr>);
+            return table;
         }
-        table.push(<tr>{row}</tr>);
-        return table;
-
     }   
 
     render() {
         return (
             <div className="container">
-            <div className = "Search">
-                <h3>Zip Code:</h3>
-                <input id="search-text" type="text" placeholder="Try 10016"/>
-                <button className="search-button" onClick={this.handleSearchClick}>Search</button>
-            </div>
-            <table className="data">
-                {this.makeTable()}
-            </table>
+                <div className = "Search">
+                    <h3>Zip Code:</h3>
+                    <input id="search-text" type="text" placeholder="Try 10016"/>
+                    <button className="search-button" onClick={this.handleSearchClick}>Search</button>
+                </div>
+                <table id="data">
+                    {this.makeTable()}
+                </table>
             </div>
         )
     }
